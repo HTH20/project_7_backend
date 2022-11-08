09ba80020b782a9a3b39882b944cd66fe7bfce0f
HTH20 <beattwobeer@gmail.com>
2022-11-08 17:06:59 +0100

    Final commit

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ async def predict(data_scoring: DataScoring):
     data_in = [[data['CODE_GENDER_F'], data['CODE_GENDER_M'], data['CODE_REJECT_REASON_CLIENT_mean'], data['CODE_REJECT_REASON_HC_mean'], data['CODE_REJECT_REASON_LIMIT_mean'], data['CODE_REJECT_REASON_SCOFR_mean'], data['CODE_REJECT_REASON_SCO_mean'], data['CODE_REJECT_REASON_SYSTEM_mean'], data['CODE_REJECT_REASON_VERIF_mean'], data['CODE_REJECT_REASON_XAP_mean'], data['CODE_REJECT_REASON_XNA_mean'], data['EXT_SOURCE_2'],data['EXT_SOURCE_3'], data['FLAG_OWN_CAR_N'], data['FLAG_OWN_CAR_Y'], data['NAME_EDUCATION_TYPE_Academic_degree'], data['NAME_EDUCATION_TYPE_Higher_education'], data['NAME_EDUCATION_TYPE_Incomplete_higher'], data['NAME_EDUCATION_TYPE_Lower_secondary'], data['NAME_EDUCATION_TYPE_Secondary_secondary_special']]]
    
     prediction = model.predict(data_in)
-    probability = model.predict_proba(data_in).max()
     return  prediction.tolist()
     
 if __name__ == "__main__":
